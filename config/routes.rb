d60cd1b5048114c57dcfdb9a7016da6dde5c7cc3
MeqdamAlqudah <memuq56@gmail.com>
2022-04-02 16:16:43 +0300

    Use resources insted of just get method

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root 'users#index'
-  get "/users/:user_id/posts(.:format)", to: "posts#index"
-  get "/users/:user_id/posts/:id(.:format)", to: "posts#show"
-  get "/users(.:format)", to: "users#index"
-  get "/users/:id(.:format)", to: "users#show"
+  # get "/users/:user_id/posts(.:format)", to: "posts#index"
+  # get "/users/:user_id/posts/:id(.:format)", to: "posts#show"
+  # get "/users(.:format)", to: "users#index"
+  # get "/users/:id(.:format)", to: "users#show"
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
 end
+end
+
+
