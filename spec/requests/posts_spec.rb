3d5463f7b5eb02ef4138798490db97e2c8c3b919
MeqdamAlqudah <memuq56@gmail.com>
2022-04-02 19:12:00 +0300

    Fix code reviewer requested changes

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-RSpec.describe PostsController, type: :controller do
+RSpec.describe "Posts", type: :request do
   describe 'GET index' do
     it 'redirects posts to index page' do
       get '/users/1/posts'
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-RSpec.describe UsersController, type: :controller do
+RSpec.describe "Users", type: :request do
   describe 'GET index' do
     it 'redirects posts to index page' do
       get '/users'
