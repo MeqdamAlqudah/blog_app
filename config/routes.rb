f44fc4d367e57253ce5101b5cf2cab54192737b3
MeqdamAlqudah <memuq56@gmail.com>
2022-04-16 15:33:19 +0300

    remove commented code

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,7 @@ Rails.application.routes.draw do
   post "users/:user_id/newpost" => "users#createpost", :as => :create_forum
   get "users/:user_id/posts/:post_id/newcomment" => "users#newcomment",:as => :forumssecond
   post "users/:user_id/posts/:post_id/createcomment" => "users#createcomment", :as => :create_forumsecond
-  # get "/users/:user_id/posts(.:format)", to: "posts#index"
-  # get "/users/:user_id/posts/:id(.:format)", to: "posts#show"
-  # get "/users(.:format)", to: "users#index"
-  # get "/users/:id(.:format)", to: "users#show"
+
   resources :users, only: [:newpost,:index, :show] do
     resources :posts, only: [:newpost,:index, :show]
 end
